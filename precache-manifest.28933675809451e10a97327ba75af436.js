30cf8f54f77171acdc0a27777db689bd86e7b51d
dani <gre4kaDISCOUNT@gmail.com>
2020-10-05 14:17:59 +0900

    Updates

---
+++ precache-manifest.28933675809451e10a97327ba75af436.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21601b4c0dfe335f894e22835f0e6be8",
+    "url": "/discord-tz/index.html"
+  },
+  {
+    "revision": "e9dc7de4ac7e38fbbdf8",
+    "url": "/discord-tz/static/css/main.93bdc339.chunk.css"
+  },
+  {
+    "revision": "dc5c46eb92e3347356d5",
+    "url": "/discord-tz/static/js/2.d20a69ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/discord-tz/static/js/2.d20a69ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9dc7de4ac7e38fbbdf8",
+    "url": "/discord-tz/static/js/main.b5ebf09e.chunk.js"
+  },
+  {
+    "revision": "6be002d4466520835b57",
+    "url": "/discord-tz/static/js/runtime-main.9127119c.js"
+  },
+  {
+    "revision": "9b0e5d1787454db5f34656874b155c05",
+    "url": "/discord-tz/static/media/Whitney-Black.9b0e5d17.woff"
+  },
+  {
+    "revision": "e8f34878595d20501035e9e74c132920",
+    "url": "/discord-tz/static/media/Whitney-Black.e8f34878.woff2"
+  },
+  {
+    "revision": "752a3bb681e31b1babc89b6ca1d7cb04",
+    "url": "/discord-tz/static/media/Whitney-Bold.752a3bb6.woff"
+  },
+  {
+    "revision": "7eb403beefb27b95ef4a6e71df682f12",
+    "url": "/discord-tz/static/media/Whitney-Bold.7eb403be.woff2"
+  },
+  {
+    "revision": "4539737d5c31eab13975c345abf8f9ba",
+    "url": "/discord-tz/static/media/Whitney-Light.4539737d.woff"
+  },
+  {
+    "revision": "baa7db9068e9472c3b6155ace6fbfc8e",
+    "url": "/discord-tz/static/media/Whitney-Light.baa7db90.woff2"
+  },
+  {
+    "revision": "805915b72a27c985687f89568a809db5",
+    "url": "/discord-tz/static/media/Whitney-Medium.805915b7.woff2"
+  },
+  {
+    "revision": "a37b6d9d61978081c039301f0a5e486f",
+    "url": "/discord-tz/static/media/Whitney-Medium.a37b6d9d.woff"
+  }
+]);
